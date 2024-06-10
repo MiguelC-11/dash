7cc0825c7daec6172047b14539d14b8dbea732fd
MiguelC-11 <166652902+MiguelC-11@users.noreply.github.com>
2024-06-10 07:27:38 -0400

    Add files via upload

---
+++ dash/principal.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bomberos UNEG</title>
+    <link rel="stylesheet" href="principal.css">
+</head>
+<body>
+    <div class="container">
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="#">
+                    <span class="icon"><ion-icon name="home-outline"></span>
+                    <span class="title">Inicio</span>
+                    </a>
+                </li>
+
+
+
+                <li>
+                    <a href="#">
+                    <span class="icon"><ion-icon name="id-card-outline"></ion-icon></span>
+                    <span class="title">Informacion personal</span>
+                    </a>
+                </li>
+
+
+
+                <li>
+                    <a href="#">
+                    <span class="icon"><ion-icon name="alarm-outline"></ion-icon></span>
+                    <span class="title">Horas de guardia</span>
+                    </a>
+                </li>
+
+
+
+                <li>
+                    <a href="#">
+                    <span class="icon"><ion-icon name="newspaper-outline"></ion-icon></span>
+                    <span class="title">asistencia</span>
+                    </a>
+                </li>
+
+
+
+                <li>
+                    <a href="#">
+                    <span class="icon"><ion-icon name="person-circle-outline"></ion-icon></span>
+                    <span class="title">Usuarios</span>
+                    </a>
+                </li>
+
+
+
+                <li>
+                    <a href="#">
+                    <span class="icon"><ion-icon name="reader-outline"></ion-icon></span>
+                    <span class="title">Respaldo de base de datos</span>
+                    </a>
+                </li>
+
+
+
+                <li>
+                    <a href="#">
+                    <span class="icon"><ion-icon name="construct-outline"></ion-icon></span>
+                    <span class="title">Configuracion</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+<script  type = "module" src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"> </script> 
+<script  nomodule src = "https://unpkg .com/ionicons@7.1.0/dist/ionicons/ionicons.js"> </script>
+
+</body>
+
+
+</html>
